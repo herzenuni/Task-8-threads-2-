35a995eaae2197ca297d0dc6ccae861968424f53
Nick Zhukov <njhuckov@gmail.com>
2017-04-27 17:32:05 +0300

    добавлено описание задания и файл с шаблоном программы
    Добавлен файл README.md, где описано задание и файл main.py, где дан
    шаблон для написания программы.

---
+++ main.py
@@ -0,0 +1,14 @@
+import threading
+
+
+def matrix_thread_multiply(X, Y):
+    """ здесь необходимо написать доктест """
+    # Z = X * Y
+    pass
+
+    return Z
+
+
+if __name__ == '__main__':
+    # здесь необходимо написать код
+    # для тестирования
